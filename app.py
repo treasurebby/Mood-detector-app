6135d729b46231a1caab053b21668680081a9a85
Ehiomhen Treasure <ehiomhentreasureruth@gmail.com>
2025-11-02 21:46:48 +0100

    Fix Python version and requirements for Render

--- app.py
+++ app.py
@@ -221,4 +221,5 @@ def predict_alias():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Listen on PORT provided by Render or default to 5000
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
